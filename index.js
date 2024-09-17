327da3cc9edf0d92bcfc995b591635923eb6556f
Saim <kksamali298@gmail.com>
2024-09-17 14:38:32 +0500

    Added delete button

--- index.js
+++ index.js
@@ -1,30 +1,50 @@
 
 
-document.getElementById('add').addEventListener('click',(e)=>{
+let list = document.getElementsByClassName('btn');
+
+for (let i = 0; i < list.length; i++) {
+    list[i].addEventListener('click', function () {
+        console.log(this)
+        this.parentElement.remove();
+    });
+}
+
+
+document.getElementById('add').addEventListener('click', (e) => {
     let val = document.getElementById('add').previousElementSibling.value;
     console.log(val);
-    if(!(val.length == 0)){
-       
-        additem(val); 
-        document.getElementById('add').previousElementSibling.value = '';        
-        
+    if (!(val.length == 0)) {
+
+        additem(val);
+        document.getElementById('add').previousElementSibling.value = '';
+
     }
+    let list = document.getElementsByClassName('btn');
+
+    for (let i = 0; i < list.length; i++) {
+        list[i].addEventListener('click', function () {
+            console.log(this)
+            this.parentElement.remove();
+        });
+    }
+
 
 });
 
 
-function additem(name){
 
-    let inp= '<input type="checkbox" >';
+function additem(name) {
+
+    let inp = '<input type="checkbox" >';
     let list = document.getElementById('lists');
-    let nel=document.createElement('div');
-    nel.setAttribute("class" , "lst");
-    nel.insertAdjacentHTML("afterbegin",inp);
-    
-    let nam = document.createElement('div');
-    nam.innerText=name;
-    nel.appendChild(nam);
+    let nel = document.createElement('div');
+    nel.setAttribute("class", "lst");
+    nel.insertAdjacentHTML("afterbegin", inp);
 
+    let nam = document.createElement('div');
+    nam.innerText = name;
+    nel.appendChild(nam);
+    nel.insertAdjacentHTML('beforeend', '<button class="btn">X</button>');
     list.appendChild(nel);
 }
 
